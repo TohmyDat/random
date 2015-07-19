c61e8e60f9232c9b73deabe8071b3a0cf170d383
TohmyDat <tohmy.dat@gmail.com>
2015-07-19 19:46:55 +0200

    random version1

---
+++ random.js
@@ -0,0 +1,7 @@
+//Math.random() devuelve nº aleatorio entre 0 y 1
+var numero = Math.random();
+if (numero<=0.5) {
+    console.log('\n ' + numero + ' menor que 0.5 \n');
+} else {
+    console.log('\n ' + numero + ' mayor que 0.5 \n')
+}
